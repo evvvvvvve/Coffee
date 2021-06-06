f819e8314e9624b575290264cdfbef4f08ced79a
evvvvvvve <67134518+evvvvvvve@users.noreply.github.com>
2021-06-07 04:51:54 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var app=new Vue({       
+   el:'#app',  
+      data:{
+                itemList:[
+                  { id='1',
+                    itemName:'美式咖啡',
+                    price:'35' },
+                  { id='2',
+                    itemName:'拿鐵',
+                    price:'50'},
+                  { id='3',
+                    itemName:'espresso',
+                    price:'70' },
+                
+            ]
+          }
+        });
